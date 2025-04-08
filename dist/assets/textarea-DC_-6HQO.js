e53c31e999b27df3a0fc2d20a5afdee20774d041
krishnnaaaaaaaa <gangbottle8@gmail.com>
2025-04-08 19:04:00 +0530

    Add files via upload

---
+++ dist/assets/textarea-DC_-6HQO.js
@@ -0,0 +1,6 @@
+import{c as o,d as s,j as t,ad as i}from"./index-Cnfp7bYh.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=o("LoaderCircle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]),d=s.forwardRef(({className:e,...r},a)=>t.jsx("textarea",{className:i("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",e),ref:a,...r}));d.displayName="Textarea";export{c as L,d as T};
